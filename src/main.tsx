05387ba94a801af46bd3b770d1308e514f0862ea
Flavio-JS <flavim.filho007@gmail.com>
2025-03-19 16:20:29 -0300

    removed StyledThemeProvider from main.tsx as it is already being used in app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/theme-context.tsx";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { lightTheme } from "./styles/themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <StyledThemeProvider theme={lightTheme}>
-        <App />
-      </StyledThemeProvider>
+      <App />
     </ThemeProvider>
   </StrictMode>
 );
